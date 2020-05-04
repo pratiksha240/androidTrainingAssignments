52077c48e7a16ed7fa60370f16caa556338035a0
Ashwini Joshi <ashwini.joshi@mediamagictechnologies.com>
2020-05-04 11:47:00 +0530

    commited final farm fresh project

--- MyApplication4/app/src/main/java/com/example/myapplication/producer.java
+++ MyApplication4/app/src/main/java/com/example/myapplication/producer.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -60,6 +61,8 @@ public class producer extends AppCompatActivity {
                     @Override
                     public void onClick(View v){
                             Toast.makeText(producer.this,"Order has confirmed",Toast.LENGTH_LONG).show();
+                            Intent int2 = new Intent(producer.this, MainActivity.class);
+                            startActivity(int2);
                     }
                 }
         );

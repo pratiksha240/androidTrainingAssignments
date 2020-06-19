1807bb83531a747f0e3c5daab0ac2ac40fb5959f
Pratiksha Shinde <pshindeps448@gmail.com>
2020-06-19 14:57:05 +0530

    - Created fragment to show listview by using recyclerview - Completed design part.

---
+++ RecyclerViewDemo/app/src/main/java/com/example/recyclerviewdemo/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.recyclerviewdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.add(R.id.frag_layout, new recylerviewFragment(), "Recyclerview Fragment");
+        ft.commit();
+    }
+}

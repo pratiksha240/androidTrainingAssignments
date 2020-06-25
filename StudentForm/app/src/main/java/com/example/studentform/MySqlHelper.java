0460a312dad438b9e57b7d050bc04d08c8417a83
Pratiksha Shinde <pshindeps448@gmail.com>
2020-06-25 20:11:33 +0530

    - created MySqlHelper Class to create database.

---
+++ StudentForm/app/src/main/java/com/example/studentform/MySqlHelper.java
@@ -0,0 +1,27 @@
+package com.example.studentform;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class MySqlHelper extends SQLiteOpenHelper
+{
+
+    public MySqlHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db)
+    {
+        db.execSQL("create table Employee (eid integer primary key, ename text, edesignation text)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+    {
+        db.execSQL("drop table if exists Employee");
+    }
+}

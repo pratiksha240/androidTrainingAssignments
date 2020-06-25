0b317adf38ec6c6688bb38cdda43e2a8b4f029fd
Pratiksha Shinde <pshindeps448@gmail.com>
2020-06-25 20:18:48 +0530

    - Changes in MySqlHelper Class.

--- StudentForm/app/src/main/java/com/example/studentform/MySqlHelper.java
+++ StudentForm/app/src/main/java/com/example/studentform/MySqlHelper.java
@@ -9,19 +9,23 @@ import androidx.annotation.Nullable;
 public class MySqlHelper extends SQLiteOpenHelper
 {
 
-    public MySqlHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+//    private static final String EmployeeInfo = "EmployeeInfo";
+
+    public MySqlHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version)
+    {
+        super(context, "EmployeeInfo" , factory, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db)
     {
-        db.execSQL("create table Employee (eid integer primary key, ename text, edesignation text)");
+        db.execSQL("create table Employee(eid integer primary key, ename text, edesignation text)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
     {
         db.execSQL("drop table if exists Employee");
+        onCreate(db);
     }
 }

577c33af072aac67c6739e0f0d48414214b36c2c
Pratiksha Shinde <pshindeps448@gmail.com>
2020-06-30 13:22:50 +0530

    - Completed page Design part.

---
+++ RoomDatabaseDemo/app/src/main/java/com/example/roomdatabasedemo/MainActivity.java
@@ -0,0 +1,105 @@
+package com.example.roomdatabasedemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+    FragmentManager fm = getSupportFragmentManager();
+    final BroadcastReceiver br = new BroadcastReceiver()
+    {
+        @Override
+        public void onReceive(Context context, Intent intent)
+        {
+            String data = intent.getAction();
+            System.out.println("Data = " + data);
+            switch(data)
+            {
+                case "INSERT_DATA":
+                {
+                    InsertPage insertPage = new InsertPage();
+                    insertPage(insertPage, fm);
+                    break;
+                }
+                case "FIRST_PAGE" :
+                {
+                    FirstPage firstPage = new FirstPage();
+                    firstPage(firstPage, fm);
+                    break;
+                }
+                case "DELETE_PAGE" :
+                {
+                    DeletePage deletePage = new DeletePage();
+                    System.out.println("before call ..!!");
+                    deletePage(deletePage, fm, intent);
+                    System.out.println("after call ..!!");
+                    break;
+                }
+                case "UPDATE_PAGE" :
+                {
+                    UpdatePage updatePage = new UpdatePage();
+                    System.out.println("Update page call...!!");
+                    updatePage(updatePage, fm, intent);
+                    break;
+                }
+                default:
+                {
+                    System.out.println("Invalid Flag..!!");
+                    break;
+                }
+            }
+        }
+    };
+
+    LocalBroadcastManager localBroadcastManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        localBroadcastManager = LocalBroadcastManager.getInstance(this);
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("FIRST_PAGE");
+        filter.addAction("INSERT_DATA");
+        filter.addAction("UPDATE_PAGE");
+        filter.addAction("DELETE_PAGE");
+
+        localBroadcastManager.registerReceiver(br, filter);
+
+        frontPage();
+    }
+
+    private void frontPage()
+    {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.add(R.id.frag_layout, new FirstPage(), "FirstPage");
+        ft.commit();
+    }
+
+    private void updatePage(UpdatePage updatePage, FragmentManager fm, Intent intent)
+    {
+    }
+
+    private void deletePage(DeletePage deletePage, FragmentManager fm, Intent intent)
+    {
+    }
+
+    private void firstPage(FirstPage firstPage, FragmentManager fm)
+    {
+    }
+
+    private void insertPage(InsertPage insertPage, FragmentManager fm)
+    {
+    }
+}

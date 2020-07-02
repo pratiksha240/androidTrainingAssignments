c5db31d17e338ee097f114f85471863bc715208e
Pratiksha Shinde <pshindeps448@gmail.com>
2020-07-02 13:10:31 +0530

    - Designed items_layout.xml for recyclerview items.

---
+++ AsyncTaskDemo/app/src/main/java/com/example/asynctaskdemo/MainFragment.java
@@ -0,0 +1,25 @@
+package com.example.asynctaskdemo;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainFragment extends Fragment
+{
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState)
+    {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+}
